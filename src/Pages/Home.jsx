51144a24114668f8cdd924ba2167694bd2d6b096
Isadora Koga <isadorakoga710@gmail.com>
2020-11-07 16:54:30 -0300

    :memo: - Adding Journal Image

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import HomeImg from '../Images/image00071_.jpeg';
+import JournalImg from '../Images/RANHURA 3.svg'
 import '../style/Home.css';
 
 class Home extends React.Component {
@@ -9,32 +10,34 @@ class Home extends React.Component {
       <div className="home-container">
         <div className="left-container">
           <div className="home-link">
-            <span>AVNT</span>
+            <Link className="left-link" to="/">AVNT</Link>
           </div>
           <div className="image-link">
             <img src={HomeImg} alt="trauma-003" className="home-image" />
+            <legend>Trauma 003</legend>
           </div>
           <div className="nav-link">
             <div className="nav-image"></div>
             <div className="nav-bar">
-              <Link className="link" to="/about">ABOUT</Link>
+              <Link className="left-link" to="/about">ABOUT</Link>
               <br />
-              <Link className="link" to="/contact">CONTACT</Link>
+              <Link className="left-link" to="/contact">CONTACT</Link>
               <br />
-              <Link className="link" to="/submissions">SUBMISSIONS</Link>
+              <Link className="left-link" to="/submissions">SUBMISSIONS</Link>
               <br />
-              <Link className="link" to="/contributors">CONTRIBUTORS</Link>
+              <Link className="left-link" to="/contributors">CONTRIBUTORS</Link>
             </div>
           </div>
         </div>
         <div className="right-container">
-          <Link className="link" to="/network">THE NETWORK</Link>
+          <Link className="right-link" to="/network">THE NETWORK</Link>
           <br />
-          <Link className="link" to="/residents">RESIDENTS</Link>
+          <Link className="right-link" to="/residents">RESIDENTS</Link>
           <br />
-          <Link className="link" to="/journal">JOURNAL</Link>
+          <img src={JournalImg} alt="journal" width="450px" className="journal-image" />
+          <Link className="right-link journal-link" to="/journal">JOURNAL</Link>
           <br />
-          <Link className="link" to="/archives">PROJECTS ARCHIVES</Link>
+          <Link className="right-link" to="/archives">PROJECTS ARCHIVES</Link>
         </div>
       </div>
     );
